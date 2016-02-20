b4b79916e5e1034b868d88248c180ff0e854ca07
Jialue Huang <jialuehuang@gmail.com>
2016-02-20 18:42:55 -0500

    change ViewController to LoginViewController

---
+++ iOS/Macchiato/Macchiato/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Macchiato
+//
+//  Created by Jialue Huang on 1/24/16.
+//  Copyright © 2016 Jialue Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property BOOL isIdentityVerified;
+- (IBAction)loginButton:(id)sender;
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender;
+
+@end
+
